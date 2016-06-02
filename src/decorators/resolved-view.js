6b6554d3aaba131b0b4e65c1d4936b5f934238f1
bas080 <bas080@hotmail.com>
2016-06-02 11:15:01 +0200

    feat(decorators) use @resolvedView to set viewStrategy

---
+++ src/decorators/resolved-view.js
@@ -0,0 +1,10 @@
+import {Container} from 'aurelia-dependency-injection';
+import {ViewManager} from './../view-manager';
+
+export function resolvedView(namespace, view) {
+  return function resolvedViewDecorator(target) {
+    let viewManager = Container.instance.get(ViewManager);
+    target.getViewStrategy = viewManager.resolve(namespace, view);
+    return target; /* handy for testing */
+  };
+}
