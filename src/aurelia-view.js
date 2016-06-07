dc4d135919fa4365b39672f260441d38b02e1aab
bas080 <bas080@hotmail.com>
2016-06-07 15:46:25 +0200

    fix(logging): logging using aurelia-logging instead of console

--- src/aurelia-view.js
+++ src/aurelia-view.js
@@ -1,14 +1,15 @@
 import {Config} from './config';
 import {ViewManager} from './view-manager';
 import {resolvedView} from './decorators/resolved-view';
+import {LogManager} from 'aurelia-logging';
 
 function configure(aurelia, configCallback) {
   if (typeof configCallback === 'function') {
     let config = aurelia.container.get(Config);
 
     configCallback(config);
-  } else {
-    console.warn('aurelia-form', 'config takes a function');
+  } else if (configCallback) {
+    LogManager.getLogger('aurelia-form').warn('config takes a function');
   }
 }
 
