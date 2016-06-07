ec2ab3299df459a169bae04be5c76e0ec22b737e
bas080 <bas080@hotmail.com>
2016-06-07 15:28:41 +0200

    fix(config): check if configCallback is a function, warn if not

--- src/aurelia-view.js
+++ src/aurelia-view.js
@@ -3,9 +3,12 @@ import {ViewManager} from './view-manager';
 import {resolvedView} from './decorators/resolved-view';
 
 function configure(aurelia, configCallback) {
-  if (configCallback) {
+  if (typeof configCallback === 'function') {
     let config = aurelia.container.get(Config);
+
     configCallback(config);
+  } else {
+    console.warn('aurelia-form', 'config takes a function');
   }
 }
 
