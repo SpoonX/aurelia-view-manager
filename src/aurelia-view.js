a5c2729b3ac8c5c80f39847a420a2210204db08d
bas080 <bas080@hotmail.com>
2016-06-13 10:31:01 +0200

    fix(config): uses correct export from aurelia-logging

--- src/aurelia-view.js
+++ src/aurelia-view.js
@@ -1,15 +1,14 @@
 import {Config} from './config';
 import {ViewManager} from './view-manager';
 import {resolvedView} from './decorators/resolved-view';
-import {LogManager} from 'aurelia-logging';
+import {getLogger} from 'aurelia-logging';
 
 function configure(aurelia, configCallback) {
   if (typeof configCallback === 'function') {
     let config = aurelia.container.get(Config);
-
     configCallback(config);
   } else if (configCallback) {
-    LogManager.getLogger('aurelia-form').warn('config takes a function');
+    getLogger('aurelia-view').warn('config takes a function');
   }
 }
 
