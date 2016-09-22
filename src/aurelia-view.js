2b83472dde2fe5f738863e3187caa46e2ac1f32e
bas080 <bas080@hotmail.com>
2016-09-22 15:40:06 +0200

    fix(configure): checks the correct variable for type

--- src/aurelia-view.js
+++ src/aurelia-view.js
@@ -3,7 +3,7 @@ import {Config} from './config';
 export function configure(aurelia, configOrConfigure) {
   let config = aurelia.container.get(Config);
 
-  if (typeof configCallback === 'function') {
+  if (typeof configOrConfigure === 'function') {
     return configOrConfigure(config);
   }
   config.configure(configOrConfigure);
